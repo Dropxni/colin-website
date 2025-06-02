5d6c444687b38e554725b15c07221b1c69528866
Dropxni <dropxni@icloud.com>
2025-06-02 05:09:22 -0600

    test

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,31 +1,30 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Component, inject, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { ProgramsService, Program } from '../../services/products.service';
-import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements AfterViewInit {
 
   programs: Program[] = [];
+  name: string = 'Nuestros Programas';
+
   private programService = inject(ProgramsService);
   private router = inject(Router);
   private platformId = inject(PLATFORM_ID);
-
-  name: string = 'Nuestros Programas';
 
   constructor() {
     this.programs = this.programService.obtenerProgramas();
   }
 
-  goToDetail(id: string) {
-    this.router.navigateByUrl(`/program/${id}`);
+  goToDetail(id: string): void {
+    this.router.navigate(['/program', id]); // Mejor navegación segura por segmentos
   }
 
   ngAfterViewInit(): void {
